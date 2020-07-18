615caea0bf8a2d2196ad9f846582e14edac0ea9d
shedupte <shedupte@gmail.com>
2020-07-18 01:07:50 -0400

    MVC folders added

---
+++ WebApplication3/WebApplication3/Controllers/GigController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class GigController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
